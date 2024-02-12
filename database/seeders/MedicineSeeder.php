e36b7d3100f1e465be1d5e5afc48c43312e7c48f
kazuya-13579 <kazuya200113579@gmail.com>
2024-02-13 07:59:07 +0900

    シーダファイルの修正

--- database/seeders/MedicineSeeder.php
+++ database/seeders/MedicineSeeder.php
@@ -17,15 +17,15 @@ class MedicineSeeder extends Seeder
     public function run()
     {
         DB::table('medicines')->insert([
-            'name'=>'パブロンゴールドA 28包',
-            'maker'=>'大正製薬',
-            'price'=>'2057',
-            'discription'=>'かぜの諸症状（せき、たん、のどの痛み、くしゃみ、鼻みず、鼻づまり、悪寒、発熱、頭痛、関節の痛み、筋肉の痛み）の緩和',
-            'jancode'=>'4987306045149',
+            // 'name'=>'パブロンゴールドA 28包',
+            // 'maker'=>'大正製薬',
+            // 'price'=>'2057',
+            // 'discription'=>'かぜの諸症状（せき、たん、のどの痛み、くしゃみ、鼻みず、鼻づまり、悪寒、発熱、頭痛、関節の痛み、筋肉の痛み）の緩和',
+            // 'jancode'=>'4987306045149',
             // 'created_at' => new DateTime(),
             // 'updated_at' => new DateTime(),
             // 'has_stock'=>true,
-            'category_id'=>'1',
+            // 'category_id'=>'1',
             ]);
     }
 }
